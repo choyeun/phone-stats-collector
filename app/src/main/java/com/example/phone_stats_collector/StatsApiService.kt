4a728e36bc7e426deb6eca31151cea639e8ffa1c
choyeun <choyeun@users.noreply.github.com>
2026-08-29 13:42:31 +0900

    fix: IntentFilter import 누락

--- app/src/main/java/com/example/phone_stats_collector/StatsApiService.kt
+++ app/src/main/java/com/example/phone_stats_collector/StatsApiService.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
